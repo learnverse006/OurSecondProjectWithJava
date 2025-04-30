a63c93740fe5eca4961478ce64ad659b145a6e7b
Phat dep trai <choicodeforce@gmail.com>
2025-04-30 16:17:44 +0700

    day 4: update basic model and relationship

--- src/main/java/io/ourchat/model/User.java
+++ src/main/java/io/ourchat/model/User.java
@@ -1,18 +1,27 @@
 package io.ourchat.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.time.LocalDateTime;
 
-@Data
+@Data // auto set/ get
 @Entity
-@Table(name = "users") // tránh dùng từ khóa SQL 'user'
-public class User {
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User{
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
@@ -22,4 +31,12 @@ public class User {
     private String fullName;
 
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    //Todo relationShip
+    @OneToMany(mappedBy = "user")
+    private List<ConversationMember>  conversations; // các nhóm mà user đã tham gia
+
+    @OneToMany(mappedBy = "sender")
+    private List<Message> messages = new ArrayList<>();
+
 }
